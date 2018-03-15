9284300ebd2f623928e834043b756a99e8ba96f0
Sridharreddy597 <Sridhar.ramasahayam@gmail.com>
2018-03-14 23:08:47 -0500

    read me file edited

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        { title: 'Javascript : The good parts', pages: 121 },
+        { title: 'Harry Potter', pages: 103 },
+        { title: 'The Dark tower', pages: 190 },
+        { title: 'Eloquent Ruby', pages: 18 },
+
+    ]
+}
